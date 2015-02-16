89603187b040e046def7ee5dc6a9f9820c85939f
erinus <erinus.startup@gmail.com>
2015-02-16 22:52:31 +0800

    3th commit

---
+++ ESServer.Session.Store.Memcache/Memcache.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.erinus.ESServer.Session.Store
+{
+    public class Memcache : ISessionStore
+    {
+        public class Session : ISession
+        {
+            public DateTime Time;
+            public Dictionary<String, dynamic> Values;
+
+            public Boolean Has(String key)
+            {
+                return true;
+            }
+
+            public T Get<T>(String key) where T : class
+            {
+                T value = null;
+
+                return value;
+            }
+
+            public Boolean Set(String key, dynamic value)
+            {
+                return true;
+            }
+        }
+
+        public Memcache()
+        {
+
+        }
+
+        ~Memcache()
+        {
+
+        }
+
+        public ISession Add(String sessionKey)
+        {
+            Session session = new Session();
+
+
+
+            return session;
+        }
+
+        public Boolean Has(String sessionKey)
+        {
+            return true;
+        }
+
+        public ISession Get(String sessionKey)
+        {
+            return null;
+        }
+    }
+}
